b2dbbab36301b918e9389549ad76b012e6bc49bb
tamai <koumomisaki@yahoo.co.jp>
2020-11-11 15:19:08 +0900

    kansei

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def scene_1():
     c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
     for row in c.fetchall():
         word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
-        # print(str(row[0]), row[1], row[2])
     c.close()
     print(word_info)
     return render_template("scene_1.html", html_word_info = word_info)
@@ -36,96 +35,104 @@ def scene_2():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM shopping WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_2.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_2.html", html_word_info = word_info)
 
 @app.route("/scene_3")
 def scene_3():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM eatingout WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_3.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_3.html", html_word_info = word_info)
 
 @app.route("/scene_4")
 def scene_4():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM directions WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_4.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_4.html", html_word_info = word_info)
 
 @app.route("/scene_5")
 def scene_():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM conversation WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_5.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_5.html", html_word_info = word_info)
 
 @app.route("/scene_6")
 def scene_6():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM emagency_sick WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_6.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_6.html", html_word_info = word_info)
 
 @app.route("/scene_7")
 def scene_7():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM numbers WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_7.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_7.html", html_word_info = word_info)
 
 @app.route("/scene_8")
 def scene_8():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM time_date WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_8.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_8.html", html_word_info = word_info)
 
 @app.route("/scene_9")
 def scene_9():
     # flasktest.dbに接続します
     conn = sqlite3.connect("scene_words.db")
     c = conn.cursor()
-    c.execute("SELECT english, japanese, pronunciation FROM dating WHERE id = 1")
-    # 取ってきた内容を変数に格納する
-    word_info = c.fetchone()
-    # データベースの接続終了
+    word_info = []
+    c.execute('SELECT english, japanese, pronunciation from greetings ORDER BY id')
+    for row in c.fetchall():
+        word_info.append({"english":row[0],"japanese":row[1],"pronunciation":row[2]})
     c.close()
-    return render_template("scene_9.html", word_info=word_info)
+    print(word_info)
+    return render_template("scene_9.html", html_word_info = word_info)
 
 @app.errorhandler(403)
 def mistake403(code):
